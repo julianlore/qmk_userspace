26fb380d506f241a4bf16c6f20da402417d104a5
Julian Lore <julian.lore@mail.mcgill.ca>
2025-02-04 20:12:19 -0500

    Add PERMISSIVE_HOLD

--- keyboards/crkbd/rev4_1/mini/keymaps/julianlore/config.h
+++ keyboards/crkbd/rev4_1/mini/keymaps/julianlore/config.h
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define PERMISSIVE_HOLD
 #define RGB_MATRIX_DEFAULT_VAL 100 // 0 - 255
 // For per layer RGB to work on both sides
 #define SPLIT_LAYER_STATE_ENABLE
